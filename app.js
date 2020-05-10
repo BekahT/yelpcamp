bcd40bdb7b1ff449156805e1960795534f45c61f
BekahT <Rebekah.treadwell@gmail.com>
2020-05-10 16:48:09 -0400

    Update to use env vars

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-// Local DB - mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://rqu:cx0dC9EQWXmSjiRX@cluster0-bc4yv.mongodb.net/yelp_camp?retryWrites=true&w=majority", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -49,10 +48,6 @@ app.use(function(req, res, next){
 app.use(indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
-
-// app.listen(3000, '127.0.0.1', function(){
-//    console.log("YelpCamp Server has started!"); 
-// });
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
