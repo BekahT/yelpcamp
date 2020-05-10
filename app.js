702a8632066efbc4f4d2029fd75575c5f42409cf
BekahT <Rebekah.treadwell@gmail.com>
2020-05-10 16:54:30 -0400

    Add backup db url

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+// Use the env variable, but if that isn't there, use the local db
+var db = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+
+mongoose.connect(db, {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
