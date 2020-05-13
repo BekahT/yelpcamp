66a9c4abd1a056f7fa3900dd1ebf8c9e6df21f5d
BekahT <Rebekah.treadwell@gmail.com>
2020-05-12 21:18:46 -0400

    Add user profile basic functionality

--- models/user.js
+++ models/user.js
@@ -4,6 +4,8 @@ var mongoose                = require("mongoose"),
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
+    avatar: String,
+    email: String,
     isAdmin: {type: Boolean, default: false}
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-var express     = require("express"),
-    router      = express.Router(),
-    passport    = require("passport"),
-    User        = require("../models/user");
+var express = require("express"),
+    router = express.Router(),
+    passport = require("passport"),
+    User = require("../models/user"),
+    Campground = require("../models/campground");
 
 // Root Route
-router.get("/", function(req, res){
+router.get("/", function (req, res) {
     res.render("landing");
 });
 
@@ -13,32 +14,32 @@ router.get("/", function(req, res){
 // =================
 
 // Show Registration Form
-router.get("/register", function(req, res){
-   res.render("register", {page: 'register'}); 
+router.get("/register", function (req, res) {
+    res.render("register", { page: 'register' });
 });
 
 // Register New User from Form Submission
-router.post("/register", function(req, res){    
-    var newUser = new User({username: req.body.username});
-    if(req.body.adminCode === 'whitebread') {
+router.post("/register", function (req, res) {
+    var newUser = new User({ username: req.body.username, email: req.body.email, avatar: req.body["avatar"] });
+    if (req.body.adminCode === 'whitebread') {
         newUser.isAdmin = true;
     }
-    User.register(newUser, req.body.password, function(err, user){
-       if(err) {
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
-            return res.render("register", {"error": "Error: " + err.message});
-       } 
-       // Login and redirect to campgrounds
-       passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Account successfully created. Welcome to YelpCamp, " + user.username);
-           res.redirect("/campgrounds");
-       });
+            return res.render("register", { "error": "Error: " + err.message });
+        }
+        // Login and redirect to campgrounds
+        passport.authenticate("local")(req, res, function () {
+            req.flash("success", "Account successfully created. Welcome to YelpCamp, " + user.username);
+            res.redirect("/campgrounds");
+        });
     });
 });
 
 // Log in Form
-router.get("/login", function(req, res){
-   res.render("login", {page: 'login'}); 
+router.get("/login", function (req, res) {
+    res.render("login", { page: 'login' });
 });
 
 // Log user in
@@ -46,15 +47,34 @@ router.post("/login", passport.authenticate("local", {
     successRedirect: "/campgrounds",
     failureRedirect: "/login",
     failureFlash: true
-    }), function(req, res){
+}), function (req, res) {
 });
 
 // Log user out
-router.get("/logout", function(req, res){
+router.get("/logout", function (req, res) {
     req.logout();
     req.flash("success", "User has been logged out");
     res.redirect("/campgrounds");
 });
 
+// =================
+// User Profiles Route
+// =================
+
+router.get("/users/:id", function (req, res) {
+    User.findById(req.params.id, function (err, foundUser) {
+        if (err) {
+            req.flash("error", "Error finding user!");
+            res.redirect("/");
+        }
+        Campground.find().where("author.id").equals(foundUser._id).exec(function (err, foundCampgrounds) {
+            if (err) {
+                req.flash("error", "Error finding campgrounds!");
+                res.redirect("/");
+            }
+            res.render("users/show", { user: foundUser, campgrounds: foundCampgrounds });
+        })
+    });
+});
 
 module.exports = router;
