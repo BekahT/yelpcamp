d8451f2ad13261feed7331ed765762a0230cfc5f
BekahT <Rebekah.treadwell@gmail.com>
2020-05-30 17:01:05 -0400

    Update seeded campground from dev db and use async await

--- seeds.js
+++ seeds.js
@@ -65,84 +65,263 @@ var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
 
-var data = [
+var seeds = [
     {
-        name: "Cloud's Rest",
-        price: 30,
-        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        name: "Mountain Mist",
+        price: 30.50,
+        image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet pastry. Bonbon soufflé cake carrot cake jelly. Pastry powder dessert jelly-o soufflé cotton candy cake muffin. Caramels bear claw croissant wafer toffee chocolate bar muffin jelly beans lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
     },
     {
         name: "Desert Mesa",
-        price: 50,
-        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        price: 50.75,
+        image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
     },
     {
-        name: "Canyon Floor",
-        price: 45,
-        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        name: "Mountain Lake View",
+        price: 30.99,
+        image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1259&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
     },
     {
-        name: "Salmon Creek",
-        price: 35,
-        image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        name: "Sunrise Hill",
+        price: 20.50,
+        image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
+    },
+    {
+        name: "Cloud's Rest",
+        price: 15.00,
+        image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
+    },
+    {
+        name: "The Ridge",
+        price: 30.99,
+        image: "https://images.unsplash.com/photo-1483381719261-6620dfa2d28a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
+    },
+    {
+        name: "Sunrise Overlook",
+        price: 40.00,
+        image: "https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
+    },
+    {
+        name: "Vast Desert",
+        price: 12.25,
+        image: "https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
+    },
+    {
+        name: "Snowy Hilltop",
+        price: 13.99,
+        image: "https://images.unsplash.com/photo-1568872321643-14b2408cd5f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
     },
     {
         name: "Granite Hill",
-        price: 25,
-        image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        price: 18.30,
+        image: "https://images.unsplash.com/photo-1502218808493-e5fd26249efc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Evergreen Forest",
+        price: 20.75,
+        image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Green Valley",
+        price: 9.75,
+        image: "https://images.unsplash.com/photo-1488790881751-9068aa742b9b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Frozen Oasis",
+        price: 50.00,
+        image: "https://images.unsplash.com/photo-1517217451453-818405428795?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Open Sky",
+        price: 65.50,
+        image: "https://images.unsplash.com/photo-1496947850313-7743325fa58c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Tranquility Park",
+        price: 12.00,
+        image: "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
     },
     {
         name: "Mountain Goat's Rest",
-        price: 60,
-        image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        price: 19.00,
+        image: "https://images.unsplash.com/photo-1563024767-5bd8ee292c3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
     },
+    {
+        name: "Jade Forest",
+        price: 16.50,
+        image: "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Mossy Ground",
+        price: 14.00,
+        image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&auto=format&fit=crop&w=1388&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Green Getaway",
+        price: 19.00,
+        image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Starry Beach",
+        price: 14.00,
+        image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Safari Glamping",
+        price: 85.50,
+        image: "https://images.unsplash.com/photo-1478827536114-da961b7f86d2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
+        author: {
+            id: "5eb60f1f0938af14dc577620",
+            username: "Bekah"
+        }
+    },
+    {
+        name: "Canyon Floor",
+        price: 25.75,
+        image: "https://images.unsplash.com/photo-1522031153701-b3eba74004e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet. Tootsie roll candy canes chocolate bar lollipop marshmallow. Cake pudding fruitcake brownie toffee cake jelly-o. Chocolate bar fruitcake tootsie roll.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
+    },
+    {
+        name: "Pinecone Retreat",
+        price: 30.00,
+        image: "https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Cupcake ipsum dolor sit amet gummies brownie chocolate cake wafer. Caramels cotton candy biscuit topping. Macaroon carrot cake donut dessert cheesecake ice cream tootsie roll jelly-o candy.",
+        author: {
+            id: "5ebb317e1ae74b2b944dff8b",
+            username: "Admin"
+        }
+    }
 ];
 
-function seedDB() {
-    //Remove all campgrounds
-    Campground.remove({}, function (err) {
-        if (err) {
-            console.log(err);
+async function seedDB() {
+    try {
+        await Campground.remove({});
+        console.log('Campgrounds removed');
+        await Comment.remove({});
+        console.log('Comments removed');
+
+        for (const seed of seeds) {
+            let campground = await Campground.create(seed);
+            console.log('Campground created');
+            // let comment = await Comment.create(
+            //     {
+            //         text: 'This place is great, but I wish there was internet',
+            //         author: 'Admin'
+            //     }
+            // )
+            // console.log('Comment created');
+            // campground.comments.push(comment);
+            // campground.save();
+            // console.log('Comment added to campground');
         }
-        console.log("removed campgrounds!");
-        Comment.remove({}, function (err) {
-            if (err) {
-                console.log(err);
-            }
-            console.log("removed comments!");
-            //add a few campgrounds
-            data.forEach(function (seed) {
-                Campground.create(seed, function (err, campground) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("added a campground");
-                        //create a comment
-                        Comment.create(
-                            {
-                                text: "This place is great, but I wish there was internet",
-                                author: "Homer"
-                            }, function (err, comment) {
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    campground.comments.push(comment);
-                                    campground.save();
-                                    console.log("Created new comment");
-                                }
-                            });
-                     }
-                 });
-            });
-        });
-    });
-    //add a few comments
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = seedDB;
