3d201a63df0b44787a5920a8e9152002878d9997
BekahT <Rebekah.treadwell@gmail.com>
2020-05-31 14:08:29 -0400

    Make user profile ui nicer

--- seeds.js
+++ seeds.js
@@ -72,7 +72,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet pastry. Bonbon soufflé cake carrot cake jelly. Pastry powder dessert jelly-o soufflé cotton candy cake muffin. Caramels bear claw croissant wafer toffee chocolate bar muffin jelly beans lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -82,7 +82,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -92,7 +92,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1259&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -102,7 +102,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -112,7 +112,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -122,7 +122,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1483381719261-6620dfa2d28a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -132,7 +132,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -142,7 +142,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
+            id: "5ed3ef0297eb9b3620173b77",
             username: "Admin"
         }
     },
@@ -152,7 +152,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1568872321643-14b2408cd5f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -162,7 +162,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1502218808493-e5fd26249efc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -172,7 +172,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -182,7 +182,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1488790881751-9068aa742b9b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -192,7 +192,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1517217451453-818405428795?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -202,7 +202,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1496947850313-7743325fa58c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -212,7 +212,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -222,7 +222,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1563024767-5bd8ee292c3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -232,7 +232,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -242,7 +242,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&auto=format&fit=crop&w=1388&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -252,7 +252,7 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
+            id: "5ed3eef097eb9b3620173b76",
             username: "Bekah"
         }
     },
@@ -262,8 +262,8 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
-            username: "Bekah"
+            id: "5ed3ef1597eb9b3620173b78",
+            username: "RegularJane"
         }
     },
     {
@@ -272,8 +272,8 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1478827536114-da961b7f86d2?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet macaroon. Cupcake dessert chocolate bar bonbon toffee. Topping carrot cake marshmallow fruitcake fruitcake brownie sesame snaps tiramisu lollipop.",
         author: {
-            id: "5eb60f1f0938af14dc577620",
-            username: "Bekah"
+            id: "5ed3ef1597eb9b3620173b78",
+            username: "RegularJane"
         }
     },
     {
@@ -282,8 +282,8 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1522031153701-b3eba74004e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet. Tootsie roll candy canes chocolate bar lollipop marshmallow. Cake pudding fruitcake brownie toffee cake jelly-o. Chocolate bar fruitcake tootsie roll.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
-            username: "Admin"
+            id: "5ed3ef1597eb9b3620173b78",
+            username: "RegularJane"
         }
     },
     {
@@ -292,8 +292,8 @@ var seeds = [
         image: "https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
         description: "Cupcake ipsum dolor sit amet gummies brownie chocolate cake wafer. Caramels cotton candy biscuit topping. Macaroon carrot cake donut dessert cheesecake ice cream tootsie roll jelly-o candy.",
         author: {
-            id: "5ebb317e1ae74b2b944dff8b",
-            username: "Admin"
+            id: "5ed3ef1597eb9b3620173b78",
+            username: "RegularJane"
         }
     }
 ];
